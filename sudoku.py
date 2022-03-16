14545f8c2b4607bb2d20e9d0b16ddf9df55e58b9
Felipe Woo <felipe.becerra.woo@gmail.com>
2022-03-16 12:49:03 -0600

    COMMENTS

--- sudoku.py
+++ sudoku.py
@@ -7,24 +7,36 @@ from webdriver_manager.chrome import ChromeDriverManager
 import copy
 import pyautogui as pg
 
+
+
+# Clase para resolver Sudokus.
 class SolveSudoku:
+    """
+    Se solicita la url de la página de origen del sudoku.
+    """
     def __init__(self,url) -> None:
         self.__url = url
 
+    # Métodos para obtener datos de la clase.
     def get_sudoku_problem(self):
+        """Regresa el valor del tablero sin resolver."""
         return self.__board_problem
         
     def get_sudoku_solution(self):
+        """Regresa el valor del tablero con la solución"""
         return self.__board_solution
     
+    
     def get_validation(self):
+        """
+        Método para realizar todas las validaciones del problema.
+        """
         self.validate_dimension()
         self.validate_rows()
         self.validate_columns()
         self.validate_squares()
         
 
-    
     def validate_dimension(self):
         """
         Validar que la dimensión del tablero sea de 9 x 9.
@@ -40,7 +52,6 @@ class SolveSudoku:
         Validar que no se repitan los números del 1 al 9 en ninguna fila.
         """
         
-
         if board == "new__board":
             board = self.__board
 
@@ -49,7 +60,6 @@ class SolveSudoku:
                 if element != 0:
                     assert row.count(element) == 1, "El tablero tiene números repetidos."
        
-
     def validate_columns(self):
         """
         Validar que no se repitan los números del 1 al 9 en ninguna columna.
@@ -65,6 +75,7 @@ class SolveSudoku:
         board_reordered = self.__new_matrix()
         self.validate_rows(board_reordered)
 
+    # Método interno para el ordenamiento de los cuadros internos.
     def __new_matrix(self): 
         # Acomoda las listas de los cuadros en orden para armar una nueva matriz. 
         matrix = [
@@ -83,7 +94,7 @@ class SolveSudoku:
         ]
         return matrix
         
-
+    # Método interno para cambiar los cuadros a listas.
     def __square2row(self,matrix,a,b,c,d):
         # Extrae cada cuadro de la matriz.
         sq = [row[a:b] for row in matrix][c:d]
@@ -95,7 +106,8 @@ class SolveSudoku:
 
         return square_reordered  
 
-    def possible(self,y,x,n):
+    # Método interno para la evaluación de las posibles soluciones.
+    def __possible(self,y,x,n):
         for i in range(0,9):
             if self.__board[y][i]==n:
                 return False
@@ -112,11 +124,14 @@ class SolveSudoku:
         return True
 
     def solve(self):
+        """
+        Genera el valor de la solución y lo regresa por el método: get_sudoku_solution().
+        """
         for y in range(0,9):
             for x in range(0,9):
                 if self.__board[y][x]==0:
                     for n in range(1,10):
-                        if self.possible(y,x,n):
+                        if self.__possible(y,x,n):
                             self.__board[y][x] = n
                             self.solve()
                             self.__board[y][x] = 0
@@ -126,6 +141,13 @@ class SolveSudoku:
 
 
     def get_sudoku(self,difficulty="hard"):
+        """
+        Obtiene el tablero de sudoku de la página web introducida como URL.
+
+        Nota: En caso de no utilizar la página del NY Times, modificar el siguiente código.
+        """
+
+        #Configuración del Scraper
         options = Options()
         options.add_argument("start-maximized")
         options.add_experimental_option("detach", True)
@@ -136,7 +158,7 @@ class SolveSudoku:
         driver.get(url)
         driver.implicitly_wait(0.5)
 
-
+        # Obtención del tablero de Sudoku de la página web.
         sudoku_dict = {} 
 
         grid = driver.find_elements(By.CLASS_NAME, 'su-board')
@@ -148,7 +170,7 @@ class SolveSudoku:
                 if cell is None or value is None:
                     continue
                 sudoku_dict[int(cell)] = 0 if value == 'empty' else int(value)
-
+        # Generación de la matrix de datos.
         sudoku_list = []
     
         for i in range(0,81):
@@ -161,7 +183,9 @@ class SolveSudoku:
         self.__board_problem = copy.deepcopy(sudoku_list)
 
     def set_solution(self):
-        
+        """
+        Regresa la solución del tablero a la página del NY Times.
+        """
         new_line = ['down']+['left' for i in range(0,9)]
         for i in range(0,9):
             for j in range(0,9):
@@ -171,13 +195,22 @@ class SolveSudoku:
             if i != 8 : pg.typewrite(new_line)
 
 
-
+# Se genera el objeto a partir de la clase; instanciamiento.
 sudoku = SolveSudoku("https://www.nytimes.com/puzzles/sudoku/")
+# Se obtiene el tablero.
 sudoku.get_sudoku()
+# Se valida.
 sudoku.get_validation()
-print("Sudoku Problem from New York Times")
+# Si no regresa ningún error todo esta bien.
+
+# Impresión del problea.
+print("Sudoku del New York Times")
 print(np.matrix(sudoku.get_sudoku_problem()))
+# Resolución.
 sudoku.solve()
-print("Sudoku Solution")
+# Impresión del resultado.
+print("Solución del Sudoku")
 print(np.matrix(sudoku.get_sudoku_solution()))
+
+# Subir el resultado a la página del NY Times.
 sudoku.set_solution()
